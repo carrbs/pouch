7a85be89ef29635f162807ff9ab2af765a564cee
Ben Carr <carrbens@gmail.com>
2016-05-31 22:14:24 -0700

    switch to express in server.js, remove unnecessary comments

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
-//Lets require/import the HTTP module
-var http = require('http');
+var express = require('express');
+var app = express();
 
-//Lets define a port we want to listen to
-const PORT=8080; 
+const PORT=3000; 
 
-//We need a function which handles requests and send response
-function handleRequest(request, response){
+app.get('*', function (request, response){
     response.end('It Works!! Path Hit: ' + request.url);
-}
+});
 
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
+app.listen(PORT, function(){
     console.log("Server listening on: http://localhost:%s", PORT);
 });
