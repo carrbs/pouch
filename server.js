26fd5008277b395d361a6f4d801a2fc92f8df013
Ben Carr <carrbens@gmail.com>
2016-06-02 00:04:58 -0700

    add babel and nodemon for happiness. Added some es6 to server.js for verification and moved silly code into it's own function

--- server.js
+++ server.js
@@ -1,44 +1,51 @@
 var express = require('express');
 var app = express();
 
-const PORT=3000; 
+const PORT=3000;
 
-app.get('*', function (request, response) {
-    var paths = []
+app.get('*', (request, response) => {
+    var funzies = pathFunzies(request);
+    response.end(funzies);
+});
+
+app.listen(PORT, () =>
+    console.log("Server listenin' on: http://localhost:%s", PORT));
+
+
+function pathFunzies(request) {
+    var paths = [];
     paths = request.url.split('/');
-    paths.shift() // no leading '/'
+    paths.shift();// no leading '/'
     var feedback = '';
-    console.log("the length: " + paths.length);
+    console.log("dat length: " + paths.length);
     switch (paths.length) {
         case 0:
-            feedback = "hi! put some fun stuff \(like 'foo/bar'\) in the path. ^^^"
+            feedback = "hi! put some fun stuff \(like 'foo/bar'\) in the path. ^^^";
             break;
         case 1:
-            feedback = "hi " + paths[0] + "! put some fun stuff \(like 'foo/bar'\) in the path. ^^^";
+            feedback = "hi " + paths[0] + "! PUT some fun stuff \(like 'foo/bar'\) in the path. ^^^";
             break;
         default:
-            feedback = (paths[0] + ' likes ' + paths[1])
+            feedback = (paths[0] + ' likes ' + paths[1]);
             break;
     }
     if (paths.length > 2) {
         feedback = feedback.concat(handleMany(paths));
     };
-    response.end(feedback);
-});
+    return feedback;
 
-function handleMany(list) {
-    var things = ''
-    if (list.length > 2) {
-        list.splice(0,2);
-        var things = " and:\n"
-        list.forEach(function(thing) {
-            things = things.concat('\n' + thing);
-            return things;
-        });
-    }
-    return things;
 }
 
-app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:%s", PORT);
-});
+
+function handleMany(list) {
+    var allTheThings = '';
+    if (list.length > 2) {
+        list.splice(0,2);
+        var things = " and:\n";
+        list.forEach(thing => {
+            allTheThings = allTheThings.concat('\n' + thing);
+            return allTheThings;
+        });
+    }
+    return allTheThings;
+}
