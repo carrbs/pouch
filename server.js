e8fe290350a9936297e41f778f878df9ff6e670d
Ben Carr <carrbens@gmail.com>
2016-06-03 00:24:51 -0700

    add React/ReactDOM and make it work; sane pinning in package.json

--- server.js
+++ server.js
@@ -1,51 +1,23 @@
 var express = require('express');
 var app = express();
+var React = require('react');
+var ReactDOM = require('react-dom/server');
 
-const PORT=3000;
+const PORT = 3000;
 
 app.get('*', (request, response) => {
-    var funzies = pathFunzies(request);
-    response.end(funzies);
+    var example = ReactDOM.renderToString(<YarWoot />);
+    response.end(example);
 });
 
 app.listen(PORT, () =>
-    console.log("Server listenin' on: http://localhost:%s", PORT));
+    console.log("Server listenin' on: http://localhost:%s", PORT)
+);
 
-
-function pathFunzies(request) {
-    var paths = [];
-    paths = request.url.split('/');
-    paths.shift();// no leading '/'
-    var feedback = '';
-    console.log("dat length: " + paths.length);
-    switch (paths.length) {
-        case 0:
-            feedback = "hi! put some fun stuff \(like 'foo/bar'\) in the path. ^^^";
-            break;
-        case 1:
-            feedback = "hi " + paths[0] + "! PUT some fun stuff \(like 'foo/bar'\) in the path. ^^^";
-            break;
-        default:
-            feedback = (paths[0] + ' likes ' + paths[1]);
-            break;
-    }
-    if (paths.length > 2) {
-        feedback = feedback.concat(handleMany(paths));
-    };
-    return feedback;
-
-}
-
-
-function handleMany(list) {
-    var allTheThings = '';
-    if (list.length > 2) {
-        list.splice(0,2);
-        var things = " and:\n";
-        list.forEach(thing => {
-            allTheThings = allTheThings.concat('\n' + thing);
-            return allTheThings;
-        });
-    }
-    return allTheThings;
-}
+var YarWoot = React.createClass({
+  render() {
+    return <div className="yarWoot">
+       yar woot!
+    </div>
+  }
+});
