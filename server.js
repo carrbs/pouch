611e3779b7af24918402b3c1b6cce8d23a554491
Ben Carr <carrbens@gmail.com>
2016-06-03 00:50:54 -0700

    move react component to file separate from server.js

--- server.js
+++ server.js
@@ -3,21 +3,17 @@ var app = express();
 var React = require('react');
 var ReactDOM = require('react-dom/server');
 
+import {YarWoot} from './src/components';
+
 const PORT = 3000;
+
+app.listen(PORT, () =>
+    console.log("Server listenin' on: http://localhost:%s", PORT)
+);
 
 app.get('*', (request, response) => {
     var example = ReactDOM.renderToString(<YarWoot />);
     response.end(example);
 });
 
-app.listen(PORT, () =>
-    console.log("Server listenin' on: http://localhost:%s", PORT)
-);
 
-var YarWoot = React.createClass({
-  render() {
-    return <div className="yarWoot">
-       yar woot!
-    </div>
-  }
-});
---
+++ src/components.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+var YarWoot = React.createClass({
+  render() {
+    return <div className="yarWoot">
+       yar woot!
+    </div>
+  }
+});
+
+module.exports = { YarWoot };
