d47e0bcea8f41db5638338b174566c2d26bfc95d
Ben Carr <carrbens@gmail.com>
2016-06-06 18:42:43 -0700

    add more useful components

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
+import chalk from 'chalk';
 import express from 'express';
 import React from 'react';
 import ReactDOM from 'react-dom/server';
 
-import {YarWoot} from './src/components';
+import {Base} from './src/components';
 
 const PORT = 3000;
 var app = express();
 
 app.listen(PORT, () =>
-    console.log("Server listenin' on: http://localhost:%s", PORT)
+    console.log(chalk.green("Server listenin' on: http://localhost:%s", PORT))
 );
 
 app.get('*', (request, response) => {
-    var example = ReactDOM.renderToString(<YarWoot />);
+    var example = ReactDOM.renderToString(<Base />);
     response.end(example);
 });
-
-
--- src/components.js
+++ src/components.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
-var YarWoot = React.createClass({
+
+var Base = React.createClass({
     render() {
-        return <div className="yarWoot">
-            yar woot!
-        </div>
+        return <html>
+                <head>
+                    <title>I am a title for up here</title>
+                </head>
+                <body>
+                    <div className="base">
+                        <h1>I am a title on the Base</h1>
+                        <Box />
+                    </div>
+                </body>
+        </html>
+    }
+})
+
+
+var Box = React.createClass({
+    render() {
+        return <div className="box">
+            <h2>I'm a box</h2>
+                <img src='http://www.placekitten.com/200/200' />
+                <p>I'm a paragraph about the box.</p>
+        </div>;
     }
 });
 
-module.exports = { YarWoot };
+module.exports = { Base };
